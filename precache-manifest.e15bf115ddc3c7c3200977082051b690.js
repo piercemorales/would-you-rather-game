4e0a4934427d72fa11f040505e8f167075d37842
Pierce <nathanm0220@gmail.com>
2019-11-28 20:35:40 -0500

    Updates

---
+++ precache-manifest.e15bf115ddc3c7c3200977082051b690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d500cf082681f78ef4632ea4ab377aec",
+    "url": "/would-you-rather-game/index.html"
+  },
+  {
+    "revision": "f8b50e73165e2821e00b",
+    "url": "/would-you-rather-game/static/css/main.0859133e.chunk.css"
+  },
+  {
+    "revision": "cd64e09efe38903c5abb",
+    "url": "/would-you-rather-game/static/js/2.7b98def1.chunk.js"
+  },
+  {
+    "revision": "f8b50e73165e2821e00b",
+    "url": "/would-you-rather-game/static/js/main.27ced63c.chunk.js"
+  },
+  {
+    "revision": "69a369c9515e8e3946d8",
+    "url": "/would-you-rather-game/static/js/runtime-main.1c737ab7.js"
+  }
+]);
